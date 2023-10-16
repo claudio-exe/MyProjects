20be7347de9a2d27fb88788962321d87505482e5
claudio-exe <claudio200274@gmail.com>
2023-10-16 13:38:54 +0200

    -

--- Java(MyProjects)/Encrypting/src/pacco/Mono_Alphabetic.java
+++ Java(MyProjects)/Encrypting/src/pacco/Mono_Alphabetic.java
@@ -3,29 +3,29 @@ package pacco;
 import java.util.Random;
 
 public class Mono_Alphabetic {
-        static String k = "";
-    
-    public static String genKey(){
+    static String k = "";
+
+    public static String genKey() {
         final String alfa = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         String reg = alfa;
         String key = "";
         Random rd = new Random();
-        
-        do{
-            int tmp = rd.nextInt(0,reg.length());
+
+        do {
+            int tmp = rd.nextInt(0, reg.length());
             key += reg.charAt(tmp);
-            reg = reg.replace(Character.toString(reg.charAt(tmp)),"");
-        }while(reg.length() != 0);
+            reg = reg.replace(Character.toString(reg.charAt(tmp)), "");
+        } while (reg.length() != 0);
         k = key;
         return key;
     }
 
-    public static String encrypt(String s){
+    public static String encrypt(String s) {
         StringBuilder enc = new StringBuilder();
         final String alfa = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         String key = k;
 
-        for(int i = 0; i < s.length();i++){
+        for (int i = 0; i < s.length(); i++) {
             int pos = alfa.indexOf(s.charAt(i));
             char ch = key.charAt(pos);
             enc.append(ch);
@@ -34,12 +34,12 @@ public class Mono_Alphabetic {
         return enc.toString();
     }
 
-    public static String decrypt(String s){
+    public static String decrypt(String s) {
         StringBuilder enc = new StringBuilder();
         final String alfa = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         String key = k;
 
-        for(int i = 0; i < s.length();i++){
+        for (int i = 0; i < s.length(); i++) {
             int pos = key.indexOf(s.charAt(i));
             char ch = alfa.charAt(pos);
             enc.append(ch);
@@ -49,13 +49,13 @@ public class Mono_Alphabetic {
     }
 
     public static void main(String[] args) {
-    
-        String s = "i topi non avevano nipoti";
-        System.out.println("String: "+s);
+
+        String s = "sei un coglione di merda";
+        System.out.println("String: " + s);
         genKey();
         String encr = encrypt(s);
-        System.out.println("Encrypted: "+encr);
-        System.out.println("Decrypted: "+decrypt(encr));
-        
+        System.out.println("Encrypted: " + encr);
+        System.out.println("Decrypted: " + decrypt(encr));
+
     }
 }
